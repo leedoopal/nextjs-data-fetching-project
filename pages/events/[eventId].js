50627feeb32916c9011c6d702f380fadbf8d9104
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-07-06 21:12:10 +0900

    paths for dynamic pages

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,6 @@
 import { Fragment } from 'react';
 
+import { getEventById, getAllEvents } from '../../helpers/api-util';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
@@ -30,6 +31,31 @@ function EventDetailPage(props) {
       </EventContent>
     </Fragment>
   );
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+
+  const event = await getEventById(eventId);
+  return {
+    props: {
+      selectedEvent: event,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+  const paths = events.map((event) => ({
+    params: {
+      eventId: event.id,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
 export default EventDetailPage;
